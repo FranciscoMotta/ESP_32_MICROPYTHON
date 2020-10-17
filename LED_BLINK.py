18bfa506b1fe05b88d188678f75b047e46e7215e
Julio Francisco Motta Claudio <jmottaclaudio@gmail.com>
2020-10-17 04:27:39 -0500

    Blink Led

---
+++ LED_BLINK.py
@@ -0,0 +1,14 @@
+
+# Escribe tu código aquí :-)
+
+import time
+import machine
+
+led = machine.Pin(2, machine.Pin.OUT)
+
+while True:
+    led.on()
+    time.sleep_ms(500)
+    led.off()
+    time.sleep_ms(500)
+
